06fc11312a222812017f4a4f3f6e5088b56bc544
Mohammed Hussen <mhdhsna@gmail.com>
2022-11-04 16:46:49 +0300

    Add bug on person

--- person.rb
+++ person.rb
@@ -8,9 +8,6 @@ class Person < Nameable
     super()
     @id = Random.rand(1...1000)
     @name = name
-    @age = age
-    @parent_permission = parent_permission
-    @rentals = []
   end
 
   private
