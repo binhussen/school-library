1858dc18544bc5522e31021883e5464b0eac9fbb
binhussen <mhdhsna@gmail.com>
2022-11-09 12:31:53 +0300

    Create Sample Json data

---
+++ data/person.js
@@ -0,0 +1,14 @@
+[
+    {
+        "type": "teacher",
+        "name": "Edi",
+        "age": 23,
+        "specialization": "Doctor"
+    },
+    {
+        "type": "student",
+        "name": "Mohammed",
+        "age": 24,
+        "parent_permission": false
+    }
+]
